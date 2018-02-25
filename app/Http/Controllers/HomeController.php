3130cd8592334f2e34dfe11eff11a20b846ace99
eavc531 <36776990+eavc531@users.noreply.github.com>
2018-02-25 15:28:43 +0100

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,4 +30,8 @@ class HomeController extends Controller
 
       return view('home.listSearch')->with('medicos',$medicos)->with('medicalCenters',$medicalCenters);
     }
+
+    public function prueba(){
+      
+    }
 }
