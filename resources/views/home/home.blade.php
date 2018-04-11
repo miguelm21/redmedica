1e3f003fc276ed8ee6a54301cc220725b2995729
eavc531 <eavc53189gmail.com>
2018-04-11 23:18:12 +0200

    commit test

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>cambio de prueba</p>
+    <p>cambio de prueba       </p>
+    <p>cambio de prueba       </p>
+    <p>cambio de prueba       </p>
+    <p>cambio de prueba       </p>
         @if(!isset(Auth::user()->id))
         <div class="row">
           <div class="col-lg-6 col-md-6 col-12">
